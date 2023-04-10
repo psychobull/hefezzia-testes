0eb4df021e5ce858c9227de0b4b29d475ce07d4c
psychobull <91846064+psychobull@users.noreply.github.com>
2023-04-10 11:59:40 -0300

    Delete js-base.js

--- Semana 2 - 03.04 a 07.04/js-base.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-    var textv = window.document.querySelector('input#textvel')
-    var res = window.document.querySelector('div.res')
-    var vel = Number(textv.value)
-    res.innerHTML = `<p>Sua velocidade atual é de ${vel}km/h</p>`
-    if (vel > 60){
-        res.innerHTML += `<p>Você ultrapssou o limite de velocidade</p>`
-    }
-}
